6e9ff2fe35a5414c14f6ba868da82753299c2ecf
Kenneth Chu <kennth.chu@gmail.com>
2018-03-26 23:57:59 +0800

    Initial Commit

---
+++ KCUIKit/KCUIKit.h
@@ -0,0 +1,19 @@
+//
+//  KCUIKit.h
+//  KCUIKit
+//
+//  Created by Kenneth Chu on 26/3/2018.
+//  Copyright © 2018 vegantell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KCUIKit.
+FOUNDATION_EXPORT double KCUIKitVersionNumber;
+
+//! Project version string for KCUIKit.
+FOUNDATION_EXPORT const unsigned char KCUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KCUIKit/PublicHeader.h>
+
+
